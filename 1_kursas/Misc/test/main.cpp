86ebbcf75cbfcbc5c0a9e2f0ec0cde4de11f83cf
Dizgog <93789780+Dizgog@users.noreply.github.com>
2023-10-11 17:30:36 +0300

    Revert "updated stuff"
    This reverts commit 305869879f27c90ae926e824e8357ed036040440.

--- 1_kursas/Misc/test/main.cpp
+++ 1_kursas/Misc/test/main.cpp
@@ -2,11 +2,23 @@
 #include "test.h"
 //#include "test.cpp"
 #define N 100
-#include <math.h>
 int main()
 {
-float a = 1.7285;
-float b = 1.875;
-std::cout<<(a+((b-a)/2));
+    std::vector<int> input;
+    srand(time(NULL));
+    int rnum;
+    for(int i = 0; i < N; ++i)
+    {
+
+        rnum = rand() % 1000;
+        input.push_back(rnum);
+    }
+
+
+    printVector(input);
+    bubbleSort(input);
+    printVector(input);
+
+
 
 }
